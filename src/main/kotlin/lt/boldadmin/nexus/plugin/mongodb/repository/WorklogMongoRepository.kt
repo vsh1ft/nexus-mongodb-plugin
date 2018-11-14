242c339afb2302e23b38111eccf83972adefa6cc
vsh1ft <kastytis.ch@gmail.com>
2018-11-15 01:27:19 +0200

    BA-673: renaming

--- src/main/kotlin/lt/boldadmin/nexus/plugin/mongodb/repository/WorklogMongoRepository.kt
+++ src/main/kotlin/lt/boldadmin/nexus/plugin/mongodb/repository/WorklogMongoRepository.kt
@@ -12,11 +12,7 @@ interface WorklogMongoRepository : MongoRepository<WorkLogClone, String> {
 
     fun findByProjectId(projectId: String): Collection<WorkLogClone>
 
-    fun existsByIntervalId(intervalId: String): Boolean
-
     fun findByIntervalId(intervalId: String): Collection<WorkLogClone>
-
-    fun existsByProjectIdAndCollaboratorId(projectId: String, collaboratorId: String): Boolean
 
     fun findByIntervalIdAndWorkStatusNotOrderByTimestampAsc(intervalId: String, workStatus: WorkStatus)
         : Collection<WorkLogClone>
@@ -24,4 +20,9 @@ interface WorklogMongoRepository : MongoRepository<WorkLogClone, String> {
     fun findFirstByIntervalIdAndWorkStatusOrderByTimestampDesc(intervalId: String, workStatus: WorkStatus): WorkLogClone?
 
     fun findFirstByIntervalId(intervalId: String): WorkLogClone
+
+    fun existsByIntervalId(intervalId: String): Boolean
+
+    fun existsByProjectIdAndCollaboratorId(projectId: String, collaboratorId: String): Boolean
+
 }
