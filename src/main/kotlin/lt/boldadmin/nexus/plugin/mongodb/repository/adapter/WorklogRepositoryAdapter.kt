f39a89b34e9d40723bbe3bd9eda4162718cadd35
Okaminu <aurimasdgt@gmail.com>
2019-05-23 17:37:51 +0300

    BA-884: minor improvements

--- src/main/kotlin/lt/boldadmin/nexus/plugin/mongodb/repository/adapter/WorklogRepositoryAdapter.kt
+++ src/main/kotlin/lt/boldadmin/nexus/plugin/mongodb/repository/adapter/WorklogRepositoryAdapter.kt
@@ -9,7 +9,7 @@ import org.springframework.data.domain.Sort
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.aggregation.Aggregation.match
 import org.springframework.data.mongodb.core.aggregation.Aggregation.newAggregation
-import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Criteria.where
 import org.springframework.data.mongodb.core.query.Query
 
 class WorklogRepositoryAdapter(
@@ -30,18 +30,17 @@ class WorklogRepositoryAdapter(
         worklogMongoRepository.findByCollaboratorId(collaboratorId).map { (it).get() }
 
     override fun findIntervalIdsByCollaboratorId(collaboratorId: String) =
-        findIntervalIds("collaborator.\$id", collaboratorId)
+        findWorklogIntervalIds("collaborator.\$id", collaboratorId)
 
-    override fun findIntervalIdsByProjectId(projectId: String) =
-        findIntervalIds("project.\$id", projectId)
+    override fun findIntervalIdsByProjectId(projectId: String) = findWorklogIntervalIds("project.\$id", projectId)
 
     override fun findByIntervalIdOrderByLatest(intervalId: String): Collection<Worklog> =
         worklogMongoRepository.findByIntervalIdOrderByTimestampAsc(intervalId).map { it.get() }
 
     override fun findLatest(collaboratorId: String, projectId: String): Worklog? {
         val query = Query().apply {
-            addCriteria(Criteria.where("project.\$id").`is`(projectId))
-            addCriteria(Criteria.where("collaborator.\$id").`is`(collaboratorId))
+            addCriteria(where("project.\$id").`is`(projectId))
+            addCriteria(where("collaborator.\$id").`is`(collaboratorId))
             with(Sort(Sort.Direction.DESC, "timestamp"))
             limit(1)
         }
@@ -54,29 +53,24 @@ class WorklogRepositoryAdapter(
     override fun doesUserHaveWorklogInterval(userId: String, intervalId: String) =
         findByIntervalId(intervalId).all { userRepositoryAdapter.doesUserHaveWorklog(userId, it) }
 
-    override fun doesCollaboratorHaveWorklogInterval(collaboratorId: String, intervalId: String) =
-        findByIntervalId(intervalId).all { it.collaborator.id == collaboratorId }
-
     override fun doesCollaboratorHaveWorklogIntervals(collaboratorId: String, intervalIds: Collection<String>) =
         intervalIds.all { doesCollaboratorHaveWorklogInterval(collaboratorId, it) }
+
+    override fun doesCollaboratorHaveWorklogInterval(collaboratorId: String, intervalId: String) =
+        findByIntervalId(intervalId).all { it.collaborator.id == collaboratorId }
 
     private fun findByIntervalId(intervalId: String): Collection<Worklog> =
         worklogMongoRepository.findByIntervalId(intervalId).map { it.get() }
 
-    private fun findIntervalIds(key: String, id: String): Collection<String> {
-        val aggregation = newAggregation(
-            match(
-                Criteria.where(key).`is`(id)
-                    .andOperator(Criteria.where("workStatus").`is`("START"))
-            )
-        )
-        val aggregationResults = template
-            .aggregate(
-                aggregation,
-                Worklog::class.java,
-                WorklogInterval::class.java
-            )
-        return aggregationResults.mappedResults.map { it.intervalId }.toList()
+    private fun findWorklogIntervalIds(key: String, id: String): Collection<String> {
+        return template.aggregate(
+            createAggregation(key, id),
+            Worklog::class.java,
+            WorklogInterval::class.java
+        ).mappedResults.map { it.intervalId }.toList()
     }
 
+    private fun createAggregation(key: String, id: String) = newAggregation(
+            match(where(key).`is`(id).andOperator(where("workStatus").`is`("START")))
+        )
 }
